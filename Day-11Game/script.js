68eec7baceab00b2c693fe65d0751e88f992ea84
Astha Gupta <asgupta@ingenuitygaming.com>
2022-08-18 14:47:30 +0530

    update

--- Day-11Game/script.js
+++ Day-11Game/script.js
@@ -1,43 +1,104 @@
-var h1=document.querySelector('h1');
-var startermole=document.querySelector('#mole');
-var starterdirt=document.querySelector('#mud');
-var startbtn=document.querySelector("#start");
-var points=document.querySelectorAll(".points");
-var dirt=document.querySelectorAll(".dirt");
-var mole=document.querySelectorAll(".mole");
+var h1 = document.querySelector('h1');
+var mainMole = document.getElementById('mole');
+var mainMud = document.querySelector('#mud');
+var startButton = document.querySelector('#start');
 
 
-function start()
-{
+var dirts = document.querySelectorAll('.dirt');
+var moles = document.querySelectorAll('.mole');
+var points = document.querySelectorAll('.points');
+
+//start button function
+function start(){
     h1.style.visibility="hidden";
-    startermole.style.visibility="hidden";
-    starterdirt.style.visibility="hidden";
-    startbtn.style.visibility="hidden";
-    for(var i=0;i<points.length;i++){
+    mainMole.style.visibility="hidden";
+    mainMud.style.visibility="hidden";
+    startButton.style.visibility="hidden";
+
+    for (let i = 0; i < points.length; i++) {
         points[i].style.visibility="visible";
     }
-    for(var i=0;i<6;i++){
-        dirt[i].style.visibility="visible";
+
+
+    for (let i = 0; i < dirts.length; i++) {
+        dirts[i].style.visibility="visible";
+    }
+    setInterval(()=>{
+        var i = randomIntFromInterval(1,6);
+        document.getElementById("mole"+i).style.visibility="visible";
+        document.getElementById("mole"+i).onclick=increamentbutton;
+        setTimeout(() => {
+            for(var j=0;j<moles.length;j++){
+                moles[j].style.visibility="hidden";
+            }
+        }, 900);
+    },1000);
+
+} 
+
+
+
+//random number generator
+function randomIntFromInterval(min, max) { 
+    return (Math.floor(Math.random() * (max - min + 1) + min));
+}
+  
+
+//increase the points
+var incpoints=0;
+function increamentbutton(){
+    document.getElementById("currentPoint").innerText=incpoints++;
+}
+
+
+var h1 = document.querySelector('h1');
+var mainMole = document.getElementById('mole');
+var mainMud = document.querySelector('#mud');
+var startButton = document.querySelector('#start');
+
+
+var dirts = document.querySelectorAll('.dirt');
+var moles = document.querySelectorAll('.mole');
+var points = document.querySelectorAll('.points');
+
+//start button function
+function start(){
+    h1.style.visibility="hidden";
+    mainMole.style.visibility="hidden";
+    mainMud.style.visibility="hidden";
+    startButton.style.visibility="hidden";
+
+    for (let i = 0; i < points.length; i++) {
+        points[i].style.visibility="visible";
     }
 
+
+    for (let i = 0; i < dirts.length; i++) {
+        dirts[i].style.visibility="visible";
+    }
     setInterval(()=>{
-     var i =randomnumber(1,6);
-     document.getElementById("mole"+i).style.visibility="visible";
-     document.getElementById("mole"+i).onclick=increbutton();
-     setTimeout(() =>{
-        for(var j=0;j<mole.length;j++){
-            mole[j].style.visibility="hidden";
-        }
-     },900);
-    },1500);
+        var i = randomIntFromInterval(1,6);
+        document.getElementById("mole"+i).style.visibility="visible";
+        document.getElementById("mole"+i).onclick=increamentbutton;
+        setTimeout(() => {
+            for(var j=0;j<moles.length;j++){
+                moles[j].style.visibility="hidden";
+            }
+        }, 2000);
+    },3000);
+
+} 
+
+
+
+//random number generator
+function randomIntFromInterval(min, max) { 
+    return (Math.floor(Math.random() * (max - min + 1) + min));
 }
-//     function randomTime(min,max)
-function randomnumber(min,max){
-    return(Math.floor(Math.random()*(max-min+1)+min));
-}
-//inc th score
+  
+
+//increase the points
 var incpoints=0;
-function increbutton(){
+function increamentbutton(){
     document.getElementById("currentPoint").innerText=incpoints++;
-    console.log(incpoints);
 }
